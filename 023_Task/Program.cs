3699aa0a1b085a2e95969f9f2c47569bf2c8017e
Vitaliy <vitaliysidorov@yandex.ru>
2023-01-23 14:27:16 +0500

    add Solution of Task 023

---
+++ 023_Task/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+
+int ReadNatural() // Метод проверки соответствия вводимого числа условиям задачи
+{
+    while (true)
+    {
+        Console.WriteLine("Введите натуральное число (N>0): ");
+        if (int.TryParse(Console.ReadLine(), out int natNumber) && natNumber >= 0)
+        {
+            return natNumber;
+        }
+        else Console.WriteLine("Введено нецелое или отрицательное число");
+    }
+}
+
+void powFunction(int num, int deg) // Метод вычисления таблицы чисел и вывод в консоль
+{
+    int count = 1;
+    Console.Write($"Таблица чисел {deg}-й степени от 1 до {num} -> {Math.Pow(count, deg)}");
+    count++;
+    while (count <= num)
+    {
+        Console.Write(", " + Math.Pow(count, deg));
+        count++;
+    }
+    Console.WriteLine();
+}
+
+int degree = 3;             // Показатель степени
+int n = ReadNatural();      // Ввод числа
+powFunction(n, degree);     // Вычисление и вывод таблицы чисел
